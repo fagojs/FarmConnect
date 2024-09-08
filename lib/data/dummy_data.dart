ba38b7ca40d80414fdb821a569b2853135b527d9
fagojs <sushil.phago123@gmail.com>
2024-09-08 17:05:56 +1000

    updated dummy data file

--- lib/data/dummy_data.dart
+++ lib/data/dummy_data.dart
@@ -1,6 +1,6 @@
 // data/dummy_data.dart
-import '../models/farmer_model.dart';
-import '../models/business_owner_model.dart';
+import '../../models/farmer_model.dart';
+import '../../models/business_owner_model.dart';
 
 List<Farmer> dummyFarmers = [];
 List<BusinessOwner> dummyBusinessOwner = [];
