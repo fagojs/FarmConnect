35bbd03be0f941908b455ad434514fd5f6d75d06
fagojs <sushil.phago123@gmail.com>
2024-09-08 15:11:56 +1000

    created models folder and defined data model for farmer's screens

---
+++ models/farmer_model.dart
@@ -0,0 +1,18 @@
+
+class Farmer {
+  String name;
+  String contactNumber;
+  String email;
+  String farmName;
+  String farmAddress;
+  String farmDescription;
+
+  Farmer({
+    required this.name,
+    required this.contactNumber,
+    required this.email,
+    required this.farmName,
+    required this.farmAddress,
+    required this.farmDescription,
+  });
+}
