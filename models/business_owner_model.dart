37963314306cddc80912ecb207e9bb6dbe3f4529
fagojs <sushil.phago123@gmail.com>
2024-09-08 16:43:57 +1000

    making changes to data model

--- models/business_owner_model.dart
+++ models/business_owner_model.dart
@@ -1,21 +1,11 @@
 class BusinessOwner {
-  String name;
-  String contactNumber;
   String email;
   String username;
   String password;
-  String businessName;
-  String businessAddress;
-  String businessDescription;
 
   BusinessOwner({
-    required this.name,
-    required this.contactNumber,
     required this.email,
     required this.username,
     required this.password,
-    required this.businessName,
-    required this.businessAddress,
-    required this.businessDescription,
   });
 }
--- models/farmer_model.dart
+++ models/farmer_model.dart
@@ -1,21 +1,11 @@
 class Farmer {
-  String name;
-  String contactNumber;
   String email;
   String username;
   String password;
-  String farmName;
-  String farmAddress;
-  String farmDescription;
 
   Farmer({
-    required this.name,
-    required this.contactNumber,
     required this.email,
     required this.username,
     required this.password,
-    required this.farmName,
-    required this.farmAddress,
-    required this.farmDescription,
   });
 }
