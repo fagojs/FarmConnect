f0bec08e4bd2963d09e4ccdbe74a23c90ee1a296
fagojs <sushil.phago123@gmail.com>
2024-09-08 15:57:26 +1000

    defined business-owner data model

---
+++ models/business_owner_model.dart
@@ -0,0 +1,17 @@
+class BusinessOwner {
+  String name;
+  String contactNumber;
+  String email;
+  String businessName;
+  String businessAddress;
+  String businessDescription;
+
+  BusinessOwner({
+    required this.name,
+    required this.contactNumber,
+    required this.email,
+    required this.businessName,
+    required this.businessAddress,
+    required this.businessDescription,
+  });
+}
