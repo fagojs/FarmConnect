90649d3ba0bff22a6d483efd24b59d837bf16c70
fagojs <sushil.phago123@gmail.com>
2024-09-09 16:10:49 +1000

    added user-address field

--- lib/models/create_farmer_profile.dart
+++ lib/models/create_farmer_profile.dart
@@ -4,6 +4,7 @@ class FarmerProfile {
   String fullName;
   String contactNumber;
   String email;
+  String address;
   String farmName;
   String farmAddress;
   String farmDescription;
@@ -14,6 +15,7 @@ class FarmerProfile {
     required this.fullName,
     required this.contactNumber,
     required this.email,
+    required this.address,
     required this.farmName,
     required this.farmAddress,
     required this.farmDescription,
