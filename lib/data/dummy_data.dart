4c96a8899bd2ffd28058a0ec39f18bb87cfe5a5f
fagojs <sushil.phago123@gmail.com>
2024-09-09 13:24:13 +1000

    added list to to store product data

--- lib/data/dummy_data.dart
+++ lib/data/dummy_data.dart
@@ -1,10 +1,14 @@
-// data/dummy_data.dart
+
 import '../models/farmer_model.dart';
 import '../models/business_owner_model.dart';
 import '../models/create_farmer_profile.dart';
+import '../models/product_model.dart';
 
+// in-memory list to store data
 List<Farmer> dummyFarmers = [];
 List<BusinessOwner> dummyBusinessOwner = [];
 List<FarmerProfile> farmerProfiles = [];
+List<Product> productList = [];
+
 
  
