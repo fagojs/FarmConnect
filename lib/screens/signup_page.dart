0b4182651cdc1cb1bde88351e4100f03756989ec
fagojs <sushil.phago123@gmail.com>
2024-09-09 12:38:09 +1000

    form-field validation, check credentials, save user in dummy list format

--- lib/screens/signup_page.dart
+++ lib/screens/signup_page.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+
 import './signin_page.dart';
+import '../models/business_owner_model.dart';
+import '../models/farmer_model.dart';
+import '../data/dummy_data.dart';
 
 class SignUpPage extends StatefulWidget {
   @override
@@ -9,6 +13,53 @@ class SignUpPage extends StatefulWidget {
 class _SignUpPageState extends State<SignUpPage> {
   String userType = 'Farmer';
 
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+  final _confirmPasswordController = TextEditingController();
+
+  void _register() {
+    String email = _emailController.text;
+    String password = _passwordController.text;
+    String confirmPassword = _confirmPasswordController.text;
+
+    // Check if any field is empty
+    if (email.isEmpty || password.isEmpty || confirmPassword.isEmpty) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('All fields are required!')),
+      );
+      return;
+    }
+
+    if (password != confirmPassword) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Passwords do not match')),
+      );
+      return;
+    }
+
+    if (userType == 'Farmer') {
+      // Add a new Farmer
+      Farmer newFarmer = Farmer(email: email, password: password, confirmPassword: confirmPassword);
+      setState(() {
+        dummyFarmers.add(newFarmer);
+      });
+    } else if (userType == 'Business') {
+      // Add a new Business Owner
+      BusinessOwner newBusinessOwner =
+          BusinessOwner(email: email, password: password, confirmPassword: confirmPassword);
+      setState(() {
+        dummyBusinessOwner.add(newBusinessOwner);
+      });
+    }
+
+    // Navigate to SignIn page with the correct user type
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => SignInPage(userType: userType),
+      ),
+    );
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -62,6 +113,7 @@ class _SignUpPageState extends State<SignUpPage> {
             ),
             SizedBox(height: 20),
             TextField(
+              controller: _emailController,
               decoration: InputDecoration(
                 labelText: 'Email',
                 border: OutlineInputBorder(),
@@ -69,6 +121,7 @@ class _SignUpPageState extends State<SignUpPage> {
             ),
             SizedBox(height: 10),
             TextField(
+              controller: _passwordController,
               decoration: InputDecoration(
                 labelText: 'Password',
                 border: OutlineInputBorder(),
@@ -78,6 +131,7 @@ class _SignUpPageState extends State<SignUpPage> {
             ),
             SizedBox(height: 10),
             TextField(
+              controller: _confirmPasswordController,
               decoration: InputDecoration(
                 labelText: 'Confirm Password',
                 border: OutlineInputBorder(),
@@ -87,14 +141,8 @@ class _SignUpPageState extends State<SignUpPage> {
             ),
             SizedBox(height: 20),
             ElevatedButton(
-              onPressed: () {
-                // Navigate to Sign In and pass the user type
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => SignInPage(userType: userType),
-                  ),
-                );
+              onPressed: (){
+                _register();
               },
               child: Text('Sign Up'),
               style: ElevatedButton.styleFrom(
