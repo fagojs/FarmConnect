c9beb20944446c32029d1f8dd87103a0734902ed
fagojs <sushil.phago123@gmail.com>
2024-09-09 12:44:54 +1000

    added list to store farmer and their farm information

--- lib/data/dummy_data.dart
+++ lib/data/dummy_data.dart
@@ -1,8 +1,10 @@
 // data/dummy_data.dart
-import '../../models/farmer_model.dart';
-import '../../models/business_owner_model.dart';
+import '../models/farmer_model.dart';
+import '../models/business_owner_model.dart';
+import '../models/create_farmer_profile.dart';
 
 List<Farmer> dummyFarmers = [];
 List<BusinessOwner> dummyBusinessOwner = [];
+List<FarmerProfile> farmerProfiles = [];
 
  
