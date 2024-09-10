330b5bed5f2b95fcaa5745080d002de8e5cfd886
fagojs <sushil.phago123@gmail.com>
2024-09-10 16:38:31 +1000

    list to store business profiles

--- lib/data/dummy_data.dart
+++ lib/data/dummy_data.dart
@@ -3,11 +3,13 @@ import '../models/farmer_model.dart';
 import '../models/business_owner_model.dart';
 import '../models/create_farmer_profile.dart';
 import '../models/product_model.dart';
+import '../models/create_business_profile.dart';
 
 // in-memory list to store data
 List<Farmer> dummyFarmers = [];
 List<BusinessOwner> dummyBusinessOwner = [];
 List<FarmerProfile> farmerProfiles = [];
+List<BusinessProfile> businessProfiles = [];
 List<Product> productList = [];
 
 
