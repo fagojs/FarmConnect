71879a738e7facb4a334dc1224fd1c88de0a30ec
fagojs <sushil.phago123@gmail.com>
2024-09-10 16:39:23 +1000

    addeded business specific details for currentuser

--- lib/data/currentuser.dart
+++ lib/data/currentuser.dart
@@ -5,14 +5,21 @@ class CurrentUser {
   factory CurrentUser() {
     return _instance;
   }
-
+  //common fields
   String? fullName;
   String? contactNumber;
   String? email;
   String? address;
+
+  // Farmer-Specific Fields
   String? farmName;
   String? farmAddress;
   String? farmDescription;
+
+  // Business Owner-Specific Fields
+  String? businessName;
+  String? businessAddress;
+  String? businessDescription;
 
   CurrentUser._internal();
 
