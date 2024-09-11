b7c809633fa47a3ae196267b975c61ce07471567
fagojs <sushil.phago123@gmail.com>
2024-09-11 15:36:02 +1000

    dummy data for products and categories to be displayed on business owners screens

---
+++ lib/data/all_products.dart
@@ -0,0 +1,94 @@
+// listed products by all farmers that are seen in home page of business owner
+
+final List<Map<String, dynamic>> allListedProducts = [
+  {
+    "name": "Tomatoes",
+    "quantity": "100 kg",
+    "price": "3.0",
+    "description": "Locally grown with local farm techniques.",
+    "category": "Vegetable",
+    "image": "images/tomatoes.png",
+  },
+  {
+    "name": "Milk",
+    "quantity": "200 liters",
+    "price": "1.5",
+    "description": "Fresh dairy milk from local farms.",
+    "category": "Dairy",
+    "image": "images/milk.png",
+  },
+  {
+    "name": "Strawberries",
+    "quantity": "50 kg",
+    "price": "5.0",
+    "description": "Fresh, organic strawberries picked today.",
+    "category": "Fruit",
+    "image": "images/strawberries.png",
+  },
+  {
+    "name": "Chicken Breast",
+    "quantity": "300 kg",
+    "price": "7.0",
+    "description": "Fresh, free-range chicken breast.",
+    "category": "Meat",
+    "image": "images/chicken.png",
+  },
+  {
+    "name": "Wheat",
+    "quantity": "500 kg",
+    "price": "2.0",
+    "description": "Whole wheat from local grain farmers.",
+    "category": "Grain",
+    "image": "images/wheat.png",
+  },
+  {
+    "name": "Cheese",
+    "quantity": "50 kg",
+    "price": "10.0",
+    "description": "Aged cheese from the finest dairy farms.",
+    "category": "Dairy",
+    "image": "images/cheese.png",
+  },
+  {
+    "name": "Apples",
+    "quantity": "150 kg",
+    "price": "4.0",
+    "description": "Fresh, crisp apples.",
+    "category": "Fruit",
+    "image": "images/apples.png",
+  },
+  {
+    "name": "Potatoes",
+    "quantity": "400 kg",
+    "price": "1.5",
+    "description": "Farm-fresh potatoes.",
+    "category": "Vegetable",
+    "image": "images/potatoes.png",
+  },
+  {
+    "name": "Pork Chops",
+    "quantity": "200 kg",
+    "price": "6.0",
+    "description": "Locally sourced pork chops.",
+    "category": "Meat",
+    "image": "images/pork_chops.png",
+  },
+  {
+    "name": "Corn",
+    "quantity": "350 kg",
+    "price": "1.0",
+    "description": "Sweet corn from local farms.",
+    "category": "Grains",
+    "image": "images/corn.png",
+  },
+];
+
+// list of all categories
+ final List<Map<String, String>> categories = [
+    {"name": "All", "image": "images/placeholder_img.png"},
+    {"name": "Vegetable", "image": "images/placeholder_img.png"},
+    {"name": "Dairy", "image": "images/placeholder_img.png"},
+    {"name": "Fruit", "image": "images/placeholder_img.png"},
+    {"name": "Grain", "image": "images/placeholder_img.png"},
+    {"name": "Meat", "image": "images/placeholder_img.png"},
+  ];
