959e91f20ba6fae846410e46293d97adb9b930bc
fagojs <sushil.phago123@gmail.com>
2024-09-13 17:22:26 +1000

    navigation added and code refactor

--- lib/screens/farmer/profile/profile_page.dart
+++ lib/screens/farmer/profile/profile_page.dart
@@ -34,37 +34,50 @@ class _FarmerProfilePageState extends State<FarmerProfilePage> {
               ),
             ),
             ListTile(
-              leading: Icon(Icons.home),
-              title: Text('Home'),
+              leading: const Icon(Icons.home),
+              title: const Text('Home'),
               onTap: () {
                 Navigator.pop(context);
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => FarmerHomePage()),
+                );
               }
             ),
             ListTile(
-              leading: Icon(Icons.add_circle),
-              title: Text('List product'),
+              leading: const Icon(Icons.add_circle),
+              title: const Text('List product'),
               onTap: () {
                 Navigator.pop(context);
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => ListProductPage()),
+                );
+              },
+
+            ),
+            ListTile(
+              leading: const Icon(Icons.person),
+              title: const Text('Profile'),
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => FarmerProfilePage()),
+                );
               },
             ),
             ListTile(
-              leading: Icon(Icons.person),
-              title: Text('Profile'),
+              leading: const Icon(Icons.settings),
+              title: const Text('Settings'),
               onTap: () {
                 Navigator.pop(context);
               },
             ),
-            ListTile(
-              leading: Icon(Icons.settings),
-              title: Text('Settings'),
-              onTap: () {
-                Navigator.pop(context);
-              },
-            ),
-            Divider(),
+            const Divider(),
             ListTile(
               leading: Icon(Icons.logout),
-              title: Text('Log Out'),
+              title: const Text('Log Out'),
               onTap: () {
                 Navigator.pop(context);
                 Navigator.pushReplacementNamed(context, '/signin');
@@ -79,51 +92,51 @@ class _FarmerProfilePageState extends State<FarmerProfilePage> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              CircleAvatar(
+              const CircleAvatar(
                 radius: 50,
                 backgroundImage: AssetImage('images/placeholder_img.png'), // Placeholder image
               ),
-              SizedBox(height: 10),
-              Text(currentUser.fullName ?? 'Name not provided', style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
+              const SizedBox(height: 10),
+              Text(currentUser.fullName ?? 'Name not provided', style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
               //Text('John Smith', style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
-              Text('Farmer', style: TextStyle(color: Colors.grey, fontSize: 16)),
-              SizedBox(height: 20),
+              const Text('Farmer', style: TextStyle(color: Colors.grey, fontSize: 16)),
+              const SizedBox(height: 20),
 
               // Contact Information Card
               Card(
                 child: ListTile(
-                  title: Text('Contact Information'),
+                  title: const Text('Contact Information'),
                   subtitle: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      SizedBox(height: 10),
+                      const SizedBox(height: 10),
                       Row(
                         children: [
-                          Icon(Icons.location_on, color: Colors.grey),
-                          SizedBox(width: 10),
+                          const Icon(Icons.location_on, color: Colors.grey),
+                          const SizedBox(width: 10),
                           //Text(contactInfo['location']),
                            Text(currentUser.address ?? 'Location not provided'),
                         ],
                       ),
-                      SizedBox(height: 10),
+                      const SizedBox(height: 10),
                       Row(
                         children: [
-                          Icon(Icons.phone, color: Colors.grey),
-                          SizedBox(width: 10),
+                          const Icon(Icons.phone, color: Colors.grey),
+                          const SizedBox(width: 10),
                           //Text(contactInfo['phone']),
                           Text(currentUser.contactNumber ?? 'Contact not provided'),
                         ],
                       ),
-                      SizedBox(height: 10),
+                      const SizedBox(height: 10),
                       Row(
                         children: [
-                          Icon(Icons.email, color: Colors.grey),
-                          SizedBox(width: 10),
+                          const Icon(Icons.email, color: Colors.grey),
+                          const SizedBox(width: 10),
                           //Text(contactInfo['email']),
                           Text(currentUser.email ?? 'Email not provided'),
                         ],
                       ),
-                      SizedBox(height: 10),
+                      const SizedBox(height: 10),
                     ],
                   ),
                   trailing: ElevatedButton(
@@ -134,7 +147,7 @@ class _FarmerProfilePageState extends State<FarmerProfilePage> {
                       );
                       setState(() {});
                     },
-                    child: Text('Edit'),
+                    child:const Text('Edit'),
                   ),
                 ),
               ),
@@ -142,37 +155,37 @@ class _FarmerProfilePageState extends State<FarmerProfilePage> {
               // Farm Information Card
               Card(
                 child: ListTile(
-                  title: Text('Farm Information'),
+                  title: const Text('Farm Information'),
                   subtitle: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       SizedBox(height: 10),
                       Row(
                         children: [
-                          Icon(Icons.home, color: Colors.grey),
-                          SizedBox(width: 10),
+                          const Icon(Icons.home, color: Colors.grey),
+                          const SizedBox(width: 10),
                           //Text(farmInfo['farmName']),
                           Text(currentUser.farmName ?? 'Farm name not provided'),
                         ],
                       ),
-                      SizedBox(height: 10),
+                      const SizedBox(height: 10),
                       Row(
                         children: [
-                          Icon(Icons.location_on, color: Colors.grey),
-                          SizedBox(width: 10),
+                          const Icon(Icons.location_on, color: Colors.grey),
+                          const SizedBox(width: 10),
                           Text(currentUser.farmAddress ?? 'Farm address not provided'),
                         ],
                       ),
-                      SizedBox(height: 10),
+                      const SizedBox(height: 10),
                       Row(
                         children: [
-                          Icon(Icons.info, color: Colors.grey),
-                          SizedBox(width: 10),
+                          const Icon(Icons.info, color: Colors.grey),
+                          const SizedBox(width: 10),
                          // Expanded(child: Text(farmInfo['description'])),
                           Expanded(child: Text(currentUser.farmDescription ?? 'No farm description provided')),
                         ],
                       ),
-                      SizedBox(height: 10),
+                      const SizedBox(height: 10),
                     ],
                   ),
                   trailing: ElevatedButton(
@@ -183,7 +196,7 @@ class _FarmerProfilePageState extends State<FarmerProfilePage> {
                       );
                       setState(() {});
                     },
-                    child: Text('Edit'),
+                    child: const Text('Edit'),
                   ),
                 ),
               ),
@@ -221,7 +234,7 @@ class _FarmerProfilePageState extends State<FarmerProfilePage> {
               break;
           }
         },
-        items: [
+        items: const [
           BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
           BottomNavigationBarItem(icon: Icon(Icons.add_circle), label: 'List Product'),
           BottomNavigationBarItem(icon: Icon(Icons.checklist_rtl_sharp), label: 'Orders'),
